f723016b757c5b8918282739a47b651f12d1c3eb
vnaumq <vnaumovich123@gmail.com>
2025-03-09 21:22:21 +0300

    new layers and optimizator "perfect_mode.py"

--- 2/perfect_model.py
+++ 2/perfect_model.py
@@ -5,36 +5,43 @@ import matplotlib.pyplot as plt
 from tensorflow import keras
 from tensorflow.keras.datasets import fashion_mnist
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.layers import Dense, Flatten, Dropout
 
 # Загрузка данных
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
-
+x_train = x_train.reshape((60000, 28, 28, 1))
+x_test = x_test.reshape((10000, 28, 28, 1))
 # Нормализация данных
 x_train = x_train / 255.0
 x_test = x_test / 255.0
 
 model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),
-    keras.layers.Dense(512, activation="relu"),  # Увеличиваем до 256
-    keras.layers.Dense(10, activation="softmax")
+    keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(28,28,1)),
+    keras.layers.Conv2D(64, (3,3), activation='relu'),
+    keras.layers.MaxPooling2D((2,2)),
+    keras.layers.Dropout(0.5),
+    keras.layers.Conv2D(128, (3,3), activation='relu'),
+    keras.layers.MaxPooling2D((2,2)),
+    keras.layers.Dropout(0.5),
+    keras.layers.Flatten(),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
 ])
-# Компиляция модели
-model.compile(optimizer=tf.keras.optimizers.SGD(),
+
+model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-# Обучение модели с сохранением истории
-history = model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
+history = model.fit(x_train, y_train, epochs=15, batch_size=512, shuffle=True, validation_split=0.1)
 
 # Оценка модели
 test_loss, test_acc = model.evaluate(x_test, y_test)
 print('Test accuracy:', test_acc)
 
 # Построение графиков
-# 1. График функции потерь
 plt.figure(figsize=(12, 5))
 
+# 1. График функции потерь
 plt.subplot(1, 2, 1)
 plt.plot(history.history['loss'], label='Training Loss')
 plt.plot(history.history['val_loss'], label='Validation Loss')
